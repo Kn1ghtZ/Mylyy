5750fb3fc263b68c9fe8ef8375fa44d6749d6025
luyongyong <596406242@qq.com>
2020-06-03 00:30:36 +0800

    lab10

--- lab10/config.php
+++ lab10/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DBHOST', '');
-define('DBNAME', '');
-define('DBUSER', '');
-define('DBPASS', '');
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'testuser');
+define('DBPASS', '331234567qwe');
 define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
 ?>
